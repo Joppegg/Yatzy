2fd07ccb7ef0ad245a1e3c4a142cd734b03fdca8
Jon <jo0748gr-s@student.lu.se>
2019-11-15 10:00:37 +0100

    Initial commit

---
+++ Yatzy/Dice.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yatzy
+{
+    class Dice
+    {
+        public int Value { get; set; }
+
+    }
+}
---
+++ YatzyTest/DiceTest.cs
@@ -0,0 +1,16 @@
+using NUnit.Framework;
+
+namespace YatzyTest
+{
+    [TestFixture]
+    public class Tests
+    {
+
+        [Test]
+       
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
