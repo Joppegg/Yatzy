01f7464d756a478e0766a6f524b5b604f2a85014
Jon <jo0748gr-s@student.lu.se>
2019-11-15 10:30:58 +0100

    RED

---
+++ Yatzy/Die.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yatzy
+{
+    public class Die
+    {
+        Random _random;
+        public Die(Random random)
+        {
+            _random = random;
+        }
+
+        public int Value { get; set; }
+
+
+        //This bool will be used to indicate whether the user has "saved" the dice during a round, lok
+        public bool isLocked { get; set; }
+
+
+
+    }
+}
