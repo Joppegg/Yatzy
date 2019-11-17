5b7c4e8a9a5c6d9c492ddae9d74b36a5dc8ead68
Jon <jo0748gr-s@student.lu.se>
2019-11-17 13:27:36 +0100

    RED

--- YatzyTest/ScoreParserTest.cs
+++ YatzyTest/ScoreParserTest.cs
@@ -13,7 +13,6 @@ namespace YatzyTest
     public class ScoreParserTest
     {
 
-        //If ones can be scored, should return true.
         [Test]
         [TestCase(6, 30)]
         [TestCase(4, 20)]
@@ -89,6 +88,42 @@ namespace YatzyTest
 
             var sut = new ScoreParser(mockDiceHolder.Object, correctInput);
             Assert.AreEqual(expected, sut.CalculateSingleNumbers());
+        }
+        [Test]
+        [TestCase("OnePair",2, 2, 4, 4, 1, 8)]
+        public void Calculate_ShouldCalculateMaxScoreForOnePair(string chosenScore, int inputDiceOne, int inputDiceTwo, int inputDiceThree, int inputDiceFour, int inputDiceFive, int expectedScoring)
+        {
+            //Create mockdice
+            Mock<IDie> mockDie1 = new Mock<IDie>();
+            Mock<IDie> mockDie2 = new Mock<IDie>();
+            Mock<IDie> mockDie3 = new Mock<IDie>();
+            Mock<IDie> mockDie4 = new Mock<IDie>();
+            Mock<IDie> mockDie5 = new Mock<IDie>();
+
+            mockDie1.Setup(x => x.Value).Returns(inputDiceOne);
+            mockDie2.Setup(x => x.Value).Returns(inputDiceTwo);
+            mockDie3.Setup(x => x.Value).Returns(inputDiceThree);
+            mockDie4.Setup(x => x.Value).Returns(inputDiceFour);
+            mockDie5.Setup(x => x.Value).Returns(inputDiceFive);
+
+            //Create a dicelist
+            List<IDie> diceList = new List<IDie>
+            {
+                mockDie1.Object,
+                mockDie2.Object,
+                mockDie3.Object,
+                mockDie4.Object,
+                mockDie5.Object,
+            };
+
+
+            Mock<IDiceHolder> mockDiceHolder = new Mock<IDiceHolder>();
+            mockDiceHolder.Setup(x => x.DiceList).Returns(diceList);
+
+            var sut = new ScoreParser(mockDiceHolder.Object);
+            Assert.AreEqual(expectedScoring, sut.CalculateOnePair);
+
+
         }
 
 
