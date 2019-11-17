6d7347585708ceba018cdf2509b945b5a8b35a9f
Jon <jo0748gr-s@student.lu.se>
2019-11-17 13:05:29 +0100

    REFACTOR

--- YatzyTest/ScoreParserTest.cs
+++ YatzyTest/ScoreParserTest.cs
@@ -15,7 +15,10 @@ namespace YatzyTest
 
         //If ones can be scored, should return true.
         [Test]
-        public void Calculate_ShouldReturnScoreForSelectingOnes()
+        [TestCase(6, 30)]
+        [TestCase(4, 20)]
+        [TestCase(2, 10)]
+        public void Calculate_ShouldReturnScoreForSelectingOneToSix(int input, int expected)
         {
             //Create mockdice
             Mock<IDie> mockDie1 = new Mock<IDie>();
@@ -24,13 +27,11 @@ namespace YatzyTest
             Mock<IDie> mockDie4 = new Mock<IDie>();
             Mock<IDie> mockDie5 = new Mock<IDie>();
 
-            mockDie1.Setup(x => x.Value).Returns(1);
-            mockDie2.Setup(x => x.Value).Returns(1);
-            mockDie3.Setup(x => x.Value).Returns(1);
-            mockDie4.Setup(x => x.Value).Returns(1);
-            mockDie5.Setup(x => x.Value).Returns(1);
-
-            int expected = 5;
+            mockDie1.Setup(x => x.Value).Returns(input);
+            mockDie2.Setup(x => x.Value).Returns(input);
+            mockDie3.Setup(x => x.Value).Returns(input);
+            mockDie4.Setup(x => x.Value).Returns(input);
+            mockDie5.Setup(x => x.Value).Returns(input);
 
             //Create a dicelist
             List<IDie> diceList = new List<IDie>
@@ -40,7 +41,6 @@ namespace YatzyTest
                 mockDie3.Object,
                 mockDie4.Object,
                 mockDie5.Object,
-
             };
 
             Mock<IDiceHolder> mockDiceHolder = new Mock<IDiceHolder>();
@@ -48,11 +48,7 @@ namespace YatzyTest
 
             var sut = new ScoreParser(mockDiceHolder.Object);
             Assert.AreEqual(sut.CalculateOnes(), expected);
-         
 
-
-            //Pass MockDie into MockDiceHolder
-            //Assert correct score
 
 
         }
