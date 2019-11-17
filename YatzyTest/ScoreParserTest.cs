b2bf084451d98b86c0db8b427e35ef95a5499e8e
Jon <jo0748gr-s@student.lu.se>
2019-11-17 16:38:51 +0100

    RED

--- YatzyTest/ScoreParserTest.cs
+++ YatzyTest/ScoreParserTest.cs
@@ -198,12 +198,48 @@ namespace YatzyTest
                 mockDie5.Object,
             };
 
-
             Mock<IDiceHolder> mockDiceHolder = new Mock<IDiceHolder>();
             mockDiceHolder.Setup(x => x.DiceList).Returns(diceList);
 
             var sut = new ScoreParser(mockDiceHolder.Object);
             Assert.AreEqual(expectedScoring, sut.CalculateStraight());
+
+        }
+        
+        [Test]
+        [TestCase(2, 2, 2, 2, 2, 50)]
+        [TestCase(3, 5, 6, 2, 4, 0)]
+        public void Calculate_ShouldCalculateYatzy(int inputDiceOne, int inputDiceTwo, int inputDiceThree, int inputDiceFour, int inputDiceFive, int expectedScoring)
+        {
+            //Create mockdice
+                Mock<IDie> mockDie1 = new Mock<IDie>();
+            Mock<IDie> mockDie2 = new Mock<IDie>();
+            Mock<IDie> mockDie3 = new Mock<IDie>();
+            Mock<IDie> mockDie4 = new Mock<IDie>();
+            Mock<IDie> mockDie5 = new Mock<IDie>();
+
+            mockDie1.Setup(x => x.Value).Returns(inputDiceOne);
+            mockDie2.Setup(x => x.Value).Returns(inputDiceTwo);
+            mockDie3.Setup(x => x.Value).Returns(inputDiceThree);
+            mockDie4.Setup(x => x.Value).Returns(inputDiceFour);
+            mockDie5.Setup(x => x.Value).Returns(inputDiceFive);
+
+            //Create a dicelist
+            List<IDie> diceList = new List<IDie>
+            {
+                mockDie1.Object,
+                mockDie2.Object,
+                mockDie3.Object,
+                mockDie4.Object,
+                mockDie5.Object,
+            };
+
+            Mock<IDiceHolder> mockDiceHolder = new Mock<IDiceHolder>();
+            mockDiceHolder.Setup(x => x.DiceList).Returns(diceList);
+
+            var sut = new ScoreParser(mockDiceHolder.Object);
+            Assert.AreEqual(expectedScoring, sut.CalculateYatzy);
+
 
 
         }
