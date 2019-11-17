3f57667246becb816e01ddcedfeaa513cbca8454
Jon <jo0748gr-s@student.lu.se>
2019-11-17 16:59:31 +0100

    RED

--- YatzyTest/ScoreParserTest.cs
+++ YatzyTest/ScoreParserTest.cs
@@ -206,6 +206,43 @@ namespace YatzyTest
             Assert.AreEqual(expectedScoring, sut.CalculateStraight());
 
         }
+        [TestCase(1, 2, 4, 5, 3, 15)]
+        [TestCase(3, 2, 4, 5, 5, 19)]
+        [TestCase(2, 3, 4, 5, 6, 20)]
+        [TestCase(5, 5, 5, 5, 5, 25)]
+        [Test]
+        public void Calculate_ShouldCalculateBonus(int inputDiceOne, int inputDiceTwo, int inputDiceThree, int inputDiceFour, int inputDiceFive, int expectedScoring)
+        {
+
+            //Create mockdice
+            Mock<IDie> mockDie1 = new Mock<IDie>();
+            Mock<IDie> mockDie2 = new Mock<IDie>();
+            Mock<IDie> mockDie3 = new Mock<IDie>();
+            Mock<IDie> mockDie4 = new Mock<IDie>();
+            Mock<IDie> mockDie5 = new Mock<IDie>();
+
+            mockDie1.Setup(x => x.Value).Returns(inputDiceOne);
+            mockDie2.Setup(x => x.Value).Returns(inputDiceTwo);
+            mockDie3.Setup(x => x.Value).Returns(inputDiceThree);
+            mockDie4.Setup(x => x.Value).Returns(inputDiceFour);
+            mockDie5.Setup(x => x.Value).Returns(inputDiceFive);
+
+            //Create a dicelist
+            List<IDie> diceList = new List<IDie>
+            {
+                mockDie1.Object,
+                mockDie2.Object,
+                mockDie3.Object,
+                mockDie4.Object,
+                mockDie5.Object,
+            };
+
+            Mock<IDiceHolder> mockDiceHolder = new Mock<IDiceHolder>();
+            mockDiceHolder.Setup(x => x.DiceList).Returns(diceList);
+
+            var sut = new ScoreParser(mockDiceHolder.Object);
+            Assert.AreEqual(expectedScoring, sut.CalculateChance());
+        }
         
 
 
