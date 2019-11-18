ed19a3c9467dfb83e328b8d0d902f5f2bc0e76cf
Jon <jo0748gr-s@student.lu.se>
2019-11-18 08:40:41 +0100

    RED

--- Yatzy/View/GameView.Designer.cs
+++ Yatzy/View/GameView.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Yatzy
 {
-    partial class Form1
+    partial class GameView
     {
         /// <summary>
         ///  Required designer variable.
---
+++ YatzyTest/GameTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YatzyTest
+{
+    [TestFixture]
+    class GameTest
+    {
+
+        public void SaveScore_ShouldSaveScoreInList()
+        {
+            //Arrange
+            string scoringOption = "Threes";
+            int score = 25;
+            var sut = new GameTest();
+
+            //Act
+            sut.SaveScore("Threes", 25);
+
+            //Assert
+            Assert.AreEqual(score, sut.GetScore(scoringOption));
+
+        }
+
+
+    }
+}
