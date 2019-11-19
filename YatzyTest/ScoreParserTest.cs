eb571fdbfb8ba130369086170bc97a7857631c35
Jon <jo0748gr-s@student.lu.se>
2019-11-19 14:38:16 +0100

    REFACTOR

--- YatzyTest/ScoreParserTest.cs
+++ YatzyTest/ScoreParserTest.cs
@@ -103,8 +103,8 @@ namespace YatzyTest
         {
 
             Mock<IDiceHolder> mockDiceHolder = GetMockDiceHolder(inputDiceOne, inputDiceTwo, inputDiceThree, inputDiceFour, inputDiceFive);
-
             var sut = new ScoreParser(mockDiceHolder.Object);
+
             Assert.AreEqual(expectedScoring, sut.CalculateNOfAKind(numberOfAkind));
 
         }
@@ -113,36 +113,12 @@ namespace YatzyTest
         [TestCase(3, 2, 5, 5, 5, 0)]
         [Test]
         public void Calculate_ShouldCalculateFullHouse(int inputDiceOne, int inputDiceTwo, int inputDiceThree, int inputDiceFour, int inputDiceFive, int expectedScoring)
-        {        
-            
-            //Create mockdice
-            Mock<IDie> mockDie1 = new Mock<IDie>();
-            Mock<IDie> mockDie2 = new Mock<IDie>();
-            Mock<IDie> mockDie3 = new Mock<IDie>();
-            Mock<IDie> mockDie4 = new Mock<IDie>();
-            Mock<IDie> mockDie5 = new Mock<IDie>();
+        {
 
-            mockDie1.Setup(x => x.Value).Returns(inputDiceOne);
-            mockDie2.Setup(x => x.Value).Returns(inputDiceTwo);
-            mockDie3.Setup(x => x.Value).Returns(inputDiceThree);
-            mockDie4.Setup(x => x.Value).Returns(inputDiceFour);
-            mockDie5.Setup(x => x.Value).Returns(inputDiceFive);
-
-            //Create a dicelist
-            List<IDie> diceList = new List<IDie>
-            {
-                mockDie1.Object,
-                mockDie2.Object,
-                mockDie3.Object,
-                mockDie4.Object,
-                mockDie5.Object,
-            };
-
-
-            Mock<IDiceHolder> mockDiceHolder = new Mock<IDiceHolder>();
-            mockDiceHolder.Setup(x => x.DiceList).Returns(diceList);
+            Mock<IDiceHolder> mockDiceHolder = GetMockDiceHolder(inputDiceOne, inputDiceTwo, inputDiceThree, inputDiceFour, inputDiceFive);
 
             var sut = new ScoreParser(mockDiceHolder.Object);
+
             Assert.AreEqual(expectedScoring, sut.CalculateFullHouse());
         }
 
