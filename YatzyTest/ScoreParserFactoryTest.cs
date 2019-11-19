1afa4641e14e5fbd7d6acd0715f75d2c560c8e77
Jon <jo0748gr-s@student.lu.se>
2019-11-19 12:57:10 +0100

    RED

---
+++ YatzyTest/ScoreParserFactoryTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YatzyTest
+{
+    [TestFixture]
+    class ScoreParserFactoryTest
+    {
+
+        [Test]
+        public void GetScoreParser_ShouldReturnScoreParser()
+        {
+            string typeOfParser = "Boring";
+
+            var ScoreParserFactory = new ScoreParserFactoryTest();
+
+            var actual = ScoreParserFactory.createScoreParser(typeOfParser);
+
+            Assert.IsNotNull(actual);
+           
+
+            
+
+        }
+
+
+    }
+}
